ea1176c2aa2879cec5d75e2635ba8ae3fe604d16
apc <alexcurran.va@gmail.com>
2019-07-23 15:56:09 -0500

    Finished nav and added other html pages

---
+++ script.js
@@ -0,0 +1,7 @@
+const navSetup = (() => {
+    const burgerNav = document.querySelector(".nav-img");
+    const sidebarNav = document.querySelector(".header-sidebar");
+    burgerNav.addEventListener("click", () => {
+        sidebarNav.classList.toggle("active");
+    });
+})();
