8ff5a00194f1ec965fa2ee772dec52ac38bc4b26
apc <alexcurran.va@gmail.com>
2019-07-25 11:09:48 -0500

    Fixed clickable bg link bug, and added initial opacity to make the sidebar inivisible on page load

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const navSetup = (() => {
     const burgerNav = document.querySelector(".nav-img");
     const sidebarNav = document.querySelector(".header-sidebar");
+
     burgerNav.addEventListener("click", () => {
         sidebarNav.classList.toggle("active");
     });
+
 })();
